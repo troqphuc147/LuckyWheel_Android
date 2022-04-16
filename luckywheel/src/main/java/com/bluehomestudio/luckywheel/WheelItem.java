93b1426dcadc55e5ea033c5396ad6689126e8938
troqphuc147 <phuchuynh020601@gmail.com>
2022-04-16 16:37:54 +0700

    fix wheelitem implement Serializable

--- luckywheel/src/main/java/com/bluehomestudio/luckywheel/WheelItem.java
+++ luckywheel/src/main/java/com/bluehomestudio/luckywheel/WheelItem.java
@@ -1,18 +1,24 @@
 package com.bluehomestudio.luckywheel;
 
 import android.graphics.Bitmap;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Created by mohamed on 22/04/17.
  */
 
-public class WheelItem {
+public class WheelItem implements Serializable {
 
     public int color;
     public Bitmap bitmap;
     public String text;
 
-        public WheelItem(int color, Bitmap bitmap) {
+    public WheelItem(int color, Bitmap bitmap) {
         this.color = color;
         this.bitmap = bitmap;
     }
@@ -27,4 +33,22 @@ public class WheelItem {
         this.color = color;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WheelItem wheelItem = (WheelItem) o;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return color == wheelItem.color && Objects.equals(bitmap, wheelItem.bitmap) && Objects.equals(text, wheelItem.text);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return Objects.hash(color, bitmap, text);
+        }
+        return 0;
+    }
 }
